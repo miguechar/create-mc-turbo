2b361cce8b6846760459bf7b1ed30e9ac164d094
miguechar <mbenitezchar@gmail.com>
2026-03-13 08:54:45 -0500

    renaming packages

---
+++ apps/web/src/app/(auth)/login/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { LoginForm } from '~/features/auth/components/loginForm'
+
+// export const metadata = {
+//     title: 'Log In',
+//     description: 'Log In to your account',
+// }
+
+const LogInPage = () => {
+  return (
+    <LoginForm />
+  )
+}
+
+export default LogInPage
